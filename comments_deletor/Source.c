1c7114280e76e56b93f4889a6c5f16f6fb9fd184
Nickonson <nikitabilan2002@gmail.com>
2021-12-15 02:27:40 +0300

    job is focking done

--- comments_deletor/Source.c
+++ comments_deletor/Source.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-	char temp_file_name[35], temp_str[1024];
+	char temp_file_name[35];
 	FILE *files_names;
 
 	files_names = fopen("project.txt", "r");
@@ -17,11 +17,10 @@ int main()
 
 	for (int i = 0; i <= len_numb_files; i++)
 		number_of_f = number_of_f * 10 + ((int)temp_file_name[i] - (int)'0');
-	
+
 	FILE *file_with_comments, *new_file;
 	int z;
-	short int flag_slash, flag_mlt_cmnt, flag_ct_ln_cmnt, flag_ln_cmnt,	flag_quotes, EOF_flag, flag_spec_symb_out, flag_ctln_quotes, flag_kavichka;
-	
+
 	for (int i = 0; i < number_of_f; i++)
 	{
 		fgets(temp_file_name, 35, files_names);
@@ -30,162 +29,104 @@ int main()
 		do
 		{
 			z++;
-		} while (temp_file_name[z] != '\n' && temp_file_name[z] != '\0');
-		temp_file_name[z] = '\0';
-		z--;
+		} while (temp_file_name[z] != '.');
+		temp_file_name[z + 2] = '\0';
+		z++;
 
-		
 		file_with_comments = fopen(temp_file_name, "r");
-		
+
 		//fot testing
 		//temp_file_name[0] = 'Z';
-		
+
 		//working part
 		//naming of the new file
 		temp_file_name[z] = 'w';
 		temp_file_name[z + 1] = 'c';
 		temp_file_name[z + 2] = '\0';
-		
-		// flag for different cases
-		EOF_flag = 0;
-		flag_slash = 0;
-		flag_mlt_cmnt= 0;
-		flag_ct_ln_cmnt = 0;
-		flag_ln_cmnt = 0;
-		flag_quotes = 0;
-		flag_spec_symb_out = 0;
-		flag_ctln_quotes = -1;
-		flag_kavichka = 0;
 
 		new_file = fopen(temp_file_name, "w");
-		
-		char temp_c = '1';
-		int k;
-		short int flag_EOF = 0, flag_nll;
-		while (temp_c != EOF)
+
+		char prev, last;
+		last = getc(file_with_comments);
+
+		while (!feof(file_with_comments))
 		{
-			//getting string out of the file
-			k = 0;
-			flag_nll = 0;
-			while (!flag_nll)
+			prev = last;
+			last = getc(file_with_comments);
+			if (prev == '\"')
 			{
-				if ((temp_c = getc(file_with_comments)) != EOF)
+				fputc(prev, new_file);
+				do
 				{
-					if (temp_c == '\0' || temp_c == '\n')
-						flag_nll = 1;
-					temp_str[k] = temp_c;
-					k++;
-				}
-				else
-					flag_nll = 1;
+					if (last == '\\')
+					{
+						fputc(last, new_file);
+						last = getc(file_with_comments);
+						fputc(last, new_file);
+						last = getc(file_with_comments);
+					}
+					prev = last;
+					last = getc(file_with_comments);
+					fputc(prev, new_file);
+
+				} while (prev != '\"');
 			}
-			temp_str[k + 1] = '\0'; temp_str[k + 1] = '\0'; //seems like doesnt work
-			
-			for (int t = 0; t < k; t++)
+			else if (prev == '\'')
 			{
-				/*if (temp_str[t] == '\"')
+				fputc(prev, new_file);
+				do
 				{
-					if (t > 0 && temp_str[t - 1] == '\\')
-						flag_spec_symb_out = 1;
-					if (flag_slash == 0 && flag_mlt_cmnt == 0 && flag_spec_symb_out == 0)
-						flag_quotes = (flag_quotes + 1) % 2;				
-
-					flag_spec_symb_out = 0;
-				}
-				else if (flag_quotes == 0 && flag_mlt_cmnt == 0 &&  t + 1 < k && temp_str[t] == '/' && temp_str[t + 1] == '/')
-				{			
-					flag_slash = 1;
-					while(t < k && temp_str[t] == '/')
-						t += 1;	
-				}
-				else if (flag_quotes == 0 && flag_slash == 0 && t + 1 < k)
-				{
-					if (temp_str[t] == '/' && temp_str[t + 1] == '*')
+					if (last == '\\')
 					{
-						flag_mlt_cmnt = 1;
-
+						fputc(last, new_file);
+						last = getc(file_with_comments);
+						fputc(last, new_file);
+						last = getc(file_with_comments);
 					}
-					else if (temp_str[t] == '*' && temp_str[t + 1] == '/')
-					{
-						flag_mlt_cmnt = 0;
-						t += 2;
-					}	
-				}*/
-				
+					prev = last;
+					last = getc(file_with_comments);
+					fputc(prev, new_file);
 
-				if (temp_str[t] == '\"')
+				} while (prev != '\'');
+			}
+			else if (prev == '/')
+			{
+				if (last == '/')
 				{
-					if (flag_ctln_quotes > -1 && t > 0 && temp_str[t - 1] == '\\')
-						flag_spec_symb_out = 1;
-					if (flag_slash == 0 && flag_mlt_cmnt == 0 && flag_spec_symb_out == 0)
-						flag_quotes = (flag_quotes + 1) % 2;
-
-					flag_spec_symb_out = 0;
-				}
-				else if (temp_str[t] == '\'' && flag_quotes == 0 && flag_slash == 0 && flag_mlt_cmnt == 0)
-				{
-					if(flag_ctln_quotes == -1)
-						flag_kavichka = (flag_kavichka + 1) % 2;
-				}
-				else if (flag_kavichka == 0 && flag_quotes == 0 && flag_slash == 0 && t + 1 < k)
-				{
-					if (flag_mlt_cmnt == 0 && temp_str[t] == '/' && temp_str[t + 1] == '/')
+					while (prev != '\n' && !feof(file_with_comments))
 					{
-						flag_slash = 1;
-						while (t < k && temp_str[t] == '/')
-							t += 1;
-					}
-					else
-					{
-						if (temp_str[t] == '/' && temp_str[t + 1] == '*')
+						prev = last;
+						last = getc(file_with_comments);
+						if (last == '\n')
 						{
-							flag_mlt_cmnt = 1;
-
-						}
-						else if (temp_str[t] == '*' && temp_str[t + 1] == '/')
-						{
-							flag_mlt_cmnt = 0;
-							t += 2;
+							
+							if (prev == '\\')
+							{
+								prev = getc(file_with_comments);
+								last = getc(file_with_comments);
+							}
+							fputc('\n', new_file);
 						}
 					}
 				}
-				//checking for line cutting letter
-				if (flag_slash == 1 && temp_str[t] == '\\' && temp_str[t + 1]== '\n')
-					flag_ct_ln_cmnt = 1;
+				else if (last == '*')
+				{
+					prev = getc(file_with_comments);
+					last = getc(file_with_comments);
 
-				//writing a letter to the new file
-				if (t < k)
-				{
-					if (!flag_slash && !flag_mlt_cmnt || temp_str[t] == '\n')
-						putc(temp_str[t], new_file);
-				}
-				else
-				{
-					//for the case of "*/" at the end of file
-					putc('\n', new_file);
-				}
-				if (flag_quotes == 1)
-				{
-					if (flag_ctln_quotes == -1 && temp_str[t] == '\\')
-						flag_ctln_quotes = t;
-					else
+					while (!(prev == '*' && last == '/') && !feof(file_with_comments))
 					{
-						flag_ctln_quotes = -1;
+						prev = last;
+						last = getc(file_with_comments);
 					}
+					last = getc(file_with_comments);
 				}
 			}
-			if (flag_quotes == 0 && flag_ct_ln_cmnt == 1 && flag_slash == 1)
-				flag_ct_ln_cmnt = 0;
 			else
-				flag_slash = 0;
+				fputc(prev, new_file);
 		}
-		
-
 		fclose(file_with_comments);
 		fclose(new_file);
 	}
-	
 	fclose(files_names);
-	
-	//printf("%c", c);
 }
